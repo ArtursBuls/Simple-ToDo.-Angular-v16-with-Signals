16670f5b5b7df6cdb248f9a390e8232b8fc6be75
Arturs Buls <arturs.buls@gmail.com>
2023-07-27 15:23:24 +0300

    Prettier confir edited, global style changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-version-16-project';
+
+  ngOnInit(): void {
+    this.setTimeout();
+  }
+
+  setTimeout() {
+    setTimeout(() => {
+      console.log('TimeOut');
+    }, 3333);
+  }
 }
