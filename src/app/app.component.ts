a1a0262f0b307f8a1a1d1ccb7baf2d2841535221
Arturs Buls <arturs.buls@gmail.com>
2023-08-01 19:21:20 +0300

    Removed last module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { ToDosComponent } from './to-dos/to-dos.component';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  imports: [ToDosComponent]
 })
 export class AppComponent {}
