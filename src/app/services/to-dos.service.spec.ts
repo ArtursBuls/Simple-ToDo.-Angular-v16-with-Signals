008f9f391eebafaf66ef5b2ade853fe87cbd4d35
Arturs Buls <arturs.buls@gmail.com>
2023-08-10 13:17:40 +0300

    Fixed toDosService tests

--- src/app/services/to-dos.service.spec.ts
+++ src/app/services/to-dos.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-import { ToDosService } from './to-dos.service';
 import { FilterEnum } from '../types/filter.enum';
+import { ToDosService } from './to-dos.service';
 
 describe('ToDosService', () => {
   let toDosService: ToDosService;
@@ -14,28 +14,75 @@ describe('ToDosService', () => {
     toDosService.addToDo('This is a new toDo two');
     toDosService.addToDo('This is a new toDo three');
     toDosService.addToDo('This is a new toDo four');
+    toDosService.addToDo('This is a new toDo five');
   });
 
   it('should be created', () => {
     expect(toDosService).toBeTruthy();
   });
 
+  //Test toggleToDo() function;
+  it('should toggle item "isCompleted" property value from true to false and vice versa', () => {
+    const itemId = toDosService.toDosSignal()[0].id;
+    const isCompletedValueBefore = toDosService.toDosSignal()[0].isCompleted;
+    toDosService.toggleToDo(itemId); //First toggling;
+    const isCompletedValueAfter = toDosService.toDosSignal()[0].isCompleted;
+    const firstTogglingResult = isCompletedValueBefore !== isCompletedValueAfter;
+    toDosService.toggleToDo(itemId); //Second toggling;
+    const isCompletedValueAfterSecondToggling = toDosService.toDosSignal()[0].isCompleted;
+    const secondTogglingResult = isCompletedValueBefore === isCompletedValueAfterSecondToggling;
+    expect(firstTogglingResult).toBeTrue();
+    expect(secondTogglingResult).toBeTrue();
+  });
+
+  //Test setFilter() function;
+  it('should set "filterSignal()" value', () => {
+    const filterEnum = FilterEnum;
+    toDosService.setFilter(filterEnum.ACTIVE);
+    const filterValueFirst = toDosService.filterSignal();
+    expect(filterValueFirst).toBe('active');
+    toDosService.setFilter(filterEnum.ALL);
+    const filterValueSecond = toDosService.filterSignal();
+    expect(filterValueSecond).toBe('all');
+    toDosService.setFilter(filterEnum.COMPLETED);
+    const filterValueThird = toDosService.filterSignal();
+    expect(filterValueThird).toBe('completed');
+  });
+
+  //Test does the filteredToDos() signal returns the correct data;
+  it('should render filtered signals according "filterSignal()" value', () => {
+    const filterEnum = FilterEnum;
+    const secondToDo = toDosService.toDosSignal()[1];
+    toDosService.toggleToDo(secondToDo.id);
+    const fourthToDo = toDosService.toDosSignal()[3];
+    toDosService.toggleToDo(fourthToDo.id);
+    toDosService.setFilter(filterEnum.ALL);
+    const resultAll = toDosService.filteredToDos();
+    expect(resultAll.length).toBe(5);
+    toDosService.setFilter(filterEnum.ACTIVE);
+    const resultActive = toDosService.filteredToDos();
+    expect(resultActive.length).toBe(3);
+    toDosService.setFilter(filterEnum.COMPLETED);
+    const resultCompleted = toDosService.filteredToDos();
+    expect(resultCompleted.length).toBe(2);
+  });
+
   //Test addToDo() function;
-  it('should add toDo item to the "ToDos" array', () => {
+  it('should add toDo item to the "toDosSignal()"', () => {
     const beforeAddingArrayLength = toDosService.toDosSignal().length;
     toDosService.addToDo('This is a new toDo');
     const afterAddingArrayLength = toDosService.toDosSignal().length;
-    const result = beforeAddingArrayLength < afterAddingArrayLength;
-    expect(result).toBeTrue();
+    const result = afterAddingArrayLength - beforeAddingArrayLength;
+    expect(result).toBe(1);
   });
 
   //Test editToDo() function;
   it('should change selected toDo item title', () => {
-    const originalItemTitle = toDosService.toDosSignal()[0];
-    toDosService.editToDo(originalItemTitle.id, 'This is a new item Title');
-    const modifiedItemTitle = toDosService.toDosSignal()[0];
-    const result = originalItemTitle !== modifiedItemTitle;
-    expect(result).toBeTrue();
+    const originalItem = toDosService.toDosSignal()[0];
+    const newTitle = 'This is a modified item Title';
+    toDosService.editToDo(originalItem.id, newTitle);
+    const modifiedItem = toDosService.toDosSignal()[0];
+    expect(modifiedItem.title).toBe(newTitle);
   });
 
   //Test generateRandomId() function;
@@ -52,83 +99,25 @@ describe('ToDosService', () => {
   });
 
   //Test deleteToDo function;
-  it('should remove toDo item from the "ToDos" array', () => {
+  it('should remove toDo item from the "toDosSignal()"', () => {
     const beforeDeletingArrayLength = toDosService.toDosSignal().length;
-    const itemId = toDosService.toDosSignal()[0].id;
+    const itemId = toDosService.toDosSignal()[3].id;
     toDosService.deleteToDo(itemId);
     const afterRemovingArrayLength = toDosService.toDosSignal().length;
-    const result = beforeDeletingArrayLength > afterRemovingArrayLength;
-    expect(result).toBeTrue();
-  });
-
-  //Test toggleToDo() function;
-  it('should toggle item "isCompleted" property value from true to false and vice versa', () => {
-    const itemId = toDosService.toDosSignal()[0].id;
-    const isCompletedValueBefore = toDosService.toDosSignal()[0].isCompleted;
-    //Toggle "isCompleted" property value to true(initial value is "false");
-    toDosService.toggleToDo(itemId);
-    const isCompletedValueAfter = toDosService.toDosSignal()[0].isCompleted;
-    const firstTogglingResult = isCompletedValueBefore !== isCompletedValueAfter;
-    //Toggle same item "isCompleted" property value back to false;
-    toDosService.toggleToDo(itemId);
-    const isCompletedValueAfterSecondToggle = toDosService.toDosSignal()[0].isCompleted;
-    const secondTogglingResult = isCompletedValueBefore === isCompletedValueAfterSecondToggle;
-    expect(firstTogglingResult).toBeTrue();
-    expect(secondTogglingResult).toBeTrue();
+    const result = beforeDeletingArrayLength - afterRemovingArrayLength;
+    expect(result).toBe(1);
   });
 
   //Test toggleAllToDos() function;
   it('should toggle all items "isCompleted" property value from true to false and vice versa', () => {
-    const itemId = toDosService.toDosSignal()[0].id;
-    const isCompletedValueBefore = toDosService.toDosSignal()[0].isCompleted;
-    //Toggle "isCompleted" property value to true(initial value is "false");
-    toDosService.toggleToDo(itemId);
-    const isCompletedValueAfter = toDosService.toDosSignal()[0].isCompleted;
-    const firstTogglingResult = isCompletedValueBefore !== isCompletedValueAfter;
-    //Toggle same item "isCompleted" property value back to false;
-    toDosService.toggleToDo(itemId);
-    const isCompletedValueAfterSecondToggle = toDosService.toDosSignal()[0].isCompleted;
-    const secondTogglingResult = isCompletedValueBefore === isCompletedValueAfterSecondToggle;
-    expect(firstTogglingResult).toBeTrue();
-    expect(secondTogglingResult).toBeTrue();
-  });
-
-  //Test setFilter() function;
-  it('should set filter signal value', () => {
-    const filterEnum = FilterEnum;
-    toDosService.setFilter(filterEnum.ACTIVE);
-    const filterValueFirst = toDosService.filterSignal();
-    expect(filterValueFirst).toBe('active');
-    toDosService.setFilter(filterEnum.ALL);
-    const filterValueSecond = toDosService.filterSignal();
-    expect(filterValueSecond).toBe('all');
-    toDosService.setFilter(filterEnum.COMPLETED);
-    const filterValueThird = toDosService.filterSignal();
-    expect(filterValueThird).toBe('completed');
-  });
-
-  //Test does the filteredToDos() signal returns the correct data;
-  it('should render filtered signals according "filterSignal()" value', () => {
-    const filterEnum = FilterEnum;
-    const firstToDos = toDosService.toDosSignal()[0];
-    const secondToDos = toDosService.toDosSignal()[1];
-    toDosService.toggleToDo(secondToDos.id);
-    const thirdToDos = toDosService.toDosSignal()[2];
-    const fourthToDos = toDosService.toDosSignal()[3];
-    toDosService.toggleToDo(fourthToDos.id);
-    toDosService.setFilter(filterEnum.ALL);
-    const resultAll = toDosService.filteredToDos();
-    expect(resultAll.length).toBe(4);
-    toDosService.setFilter(filterEnum.ACTIVE);
-    const resultActive = toDosService.filteredToDos();
-    expect(resultActive.length).toBe(2);
-    toDosService.setFilter(filterEnum.COMPLETED);
-    const resultCompleted = toDosService.filteredToDos();
-    expect(resultCompleted.length).toBe(2);
+    const beforeToggling = toDosService.toDosSignal().every(element => !element.isCompleted);
+    toDosService.toggleAllToDos(true);
+    const afterToggling = toDosService.toDosSignal().every(element => element.isCompleted);
+    expect(beforeToggling).toBe(afterToggling);
   });
 
   //Test deleteCompletedToDos() function;
-  it('should remove all toDos with "isCompleted" property value "true"', () => {
+  it('should remove all toDos where "isCompleted" property value is "true"', () => {
     toDosService.toggleAllToDos(true);
     toDosService.deleteCompletedToDos();
     const result = toDosService.toDosSignal();
